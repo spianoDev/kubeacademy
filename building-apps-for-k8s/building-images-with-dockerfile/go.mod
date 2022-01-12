9fb5182eea2dc72a0f5da9b2530814c5ac9172e4
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2022-01-12 15:14:38 -0600

    new app and files

---
+++ building-apps-for-k8s/building-images-with-dockerfile/go.mod
@@ -0,0 +1,5 @@
+module github.com/spianodev/kubeacademy
+
+go 1.17
+
+require github.com/ozgio/strutil v0.3.0
---
+++ building-apps-for-k8s/building-images-with-dockerfile/hello.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "github.com/ozgio/strutil"
+    "os"
+    "strconv"
+    "time"
+)
+
+func main () {
+
+    value := ""
+    if len(os.Args) > 1 {
+        value = os.Args[1]
+    } else {
+        value = os.Getenv("HELLO_ARG")
+    }
+    if len(value) == 0 {
+        value = "-h"
+    }
+
+    if value == "-h" || value == "--help" {
+        fmt.Println("USAGE: Provide a string and we'll say hello!")
+    } else {
+        output := "Hello, " + value + "!"
+        output, _ = strutil.DrawCustomBox(output, 40, strutil.Center, strutil.SimpleBox9Slice(), "\n")
+        fmt.Println(output)
+    }
+
+    IsSleepEnabled, _ := strconv.ParseBool(os.Getenv("HELLO_SLEEP"))
+    if IsSleepEnabled {
+        fmt.Printf("Sleeping 90 seconds...\n")
+        time.Sleep(90 * time.Second)
+    }
+
+}
+
+//For builds with CNB/Paketo:
+//    Using base run-image, two options (command overrides env var):
+//        docker run -e HELLO_ARG=world hello:base "hello sunshine"
+//    Using tiny run image, use:
+//        docker run -e HELLO_BOX_ARG=world hello:tiny
+
+// Initial code for kube academy coursework referenced here: https://github.com/ciberkleid/hello-go
